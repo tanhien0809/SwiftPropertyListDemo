5cbfc5fcd126bd184d760ad8ce23b41a095c498e
Cntt27 <Admin27@Cntt27s-iMac.local>
2017-05-10 14:25:22 +0700

    Initial Commit

---
+++ SwiftPropertyListDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPropertyListDemo
+//
+//  Created by Cntt27 on 5/10/17.
+//  Copyright © 2017 Cntt27. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
